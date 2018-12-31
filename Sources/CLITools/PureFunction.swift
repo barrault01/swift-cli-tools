8690b833a0c7d38411744a5838810a1948a7ab37
Antoine Barrault <barrault01@gmail.com>
2018-12-31 10:48:25 -0200

    adding optional string on print function

--- Sources/CLITools/PureFunction.swift
+++ Sources/CLITools/PureFunction.swift
@@ -1,3 +1,9 @@
 public func printAsFunction(string: String) {
     print(string)
 }
+
+public func printAsFunction(string: String?) {
+    if let str = string {
+        printAsFunction(string: str)
+    }
+}
